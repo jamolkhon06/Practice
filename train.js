393473d3f3e29dfdb5157fa0ea89713f2b595f85
Joseph <jamolxonsiddikovich@gmail.com>
2026-09-15 08:23:55 +0900

    completed G-Task

--- train.js
+++ train.js
@@ -1,3 +1,18 @@
+// G-Task
+// Yagona parametrga ega function tuzing. Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin. Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin. MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini Yuqoridagi misolda, birinchi indeksda 21 joylashgan. Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+function getHighestIndex(arr) {
+    let maxNum = arr[0];
+    let maxIndex = 0;
+    arr.forEach((element, index) => {
+        if(element > maxNum) {
+            maxNum = element
+            maxIndex = index
+        }
+    });
+    return maxIndex
+}
+
 // F-Task
 // Yagona string argumentga ega findDoublers nomli function tuzing. Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa true yokida false natija qaytarsin. MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
 
